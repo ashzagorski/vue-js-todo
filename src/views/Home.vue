9b9ea5f039a8b35a43f27128404b2441e0c8eb91
ashzagorski <ash.zagorski@gmail.com>
2018-10-16 21:17:58 -0500

    creates todo app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,95 @@
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <h1>{{ message }}</h1>
+
+    <div> <h4>Number of total tasks todo: {{ numberOfIncompleteTasks() }} </h4></div>
+    
+      <div v-for="task in tasks">
+        <div v-bind:class="{strike: task.completed}">
+          <h2 v-on:click="toggleTask(task)"> {{ task.text }} </h2>
+        </div>
+      </div>
+
+  <div> <button v-on:click="deleteTask()">Delete Completed Tasks</button> </div>
+
+<div>
+  <h3>Create New Task</h3>
+  Task: <input v-model="newTask.text">
+  Id: <input v-model="newTask.id">
+  <button v-on:click="addTask()">Add Task</button>
+</div>
+
+    </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+body {
+  background-color: white;
+  color: lightblue;
 }
+
+.strike {
+  text-decoration: line-through;
+}
+</style>
+
+<script>
+
+   var axios = require("axios");
+   
+export default {
+  data: function() {
+    return {
+      message: "Tasks Todo",
+      tasks: [],
+
+      newTask: {text: "", id: "", completed: false}
+
+
+    };
+  },
+  created: function() {
+    axios.get("http://localhost:3000/api/tasks").then(function(response) {
+      this.tasks = response.data;
+    }.bind(this));
+  },
+  methods: {
+    toggleTask: function(inputTask) {
+        inputTask.completed = !inputTask.completed;
+      },
+    addTask: function() {
+      if (this.newTask.text!== "") {
+        this.tasks.push(this.newTask);
+        this.newTask = {text: "", id: "", completed: false}; 
+        };
+       },   
+    completeTask: function(inputTask) {
+      var index = this.tasks.indexOf(inputTask);
+      this.tasks.splice(index, 1);
+      },
+    numberOfIncompleteTasks: function() {
+      var count = 0
+      this.tasks.forEach(function(task) {
+        if (!task.completed) {
+          count ++;
+        }
+      });
+      return count
+    },
+    deleteTask: function() {
+      var incompleteTasks = [];
+      for(var i = 0; i < this.tasks.length; i++) {
+        var task = task[i];
+
+        if (task.completed) {
+          incompleteTasks.push(task);
+        }
+      }
+      this.tasks = incompleteTasks;
+      },
+  },
+  computed: {}
+};
 </script>
